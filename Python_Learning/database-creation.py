151fc239bc08f619259a1529c5cd01655334ee0d
Julian Stahl <julsjunior@gmail.com>
2018-08-08 16:52:56 -0600

    restructured whole file

--- Python_Learning/database-creation.py
+++ Python_Learning/database-creation.py
@@ -2,19 +2,68 @@ import sqlite3
 import os
 from sqlite3 import Error
 
-nodeName = os.getlogin() + '.db'
-
-conn = sqlite3.connect(str(nodeName))
-cursor = conn.cursor()
 
 
-sql_create_projects_table = """ CREATE TABLE IF NOT EXISTS Events (
-                                        id integer PRIMARY KEY,
-                                        ISO_Week text,
-                                        Event_ISO_Date text,
-                                        Unix_Time real,
-                                        Event_Type text
-                                        Duration real,
-                                    ); """
+
+
+if conn is not None:
+    conn.execute(sql_create_projects_table)
+else:
+    print("Error! cannot create the database connection.")
 
 conn.close()
+
+
+import sqlite3
+from sqlite3 import Error
+
+
+ddef create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+
+    return None
+
+def create_table(conn, create_table_sql):
+    """ create a table from the create_table_sql statement
+    :param conn: Connection object
+    :param create_table_sql: a CREATE TABLE statement
+    :return:
+    """
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+ def main():
+    nodeName = os.getlogin() + '.db'
+    database = nodeName
+
+    sql_create_events_table = """ CREATE TABLE IF NOT EXISTS Events (
+                                            id integer PRIMARY KEY,
+                                            ISO_Week text,
+                                            Event_ISO_Date text,
+                                            Unix_Time real,
+                                            Event_Type text
+                                            Duration real,
+                                        ); """
+
+    # create a database connection
+    conn = create_connection(database)
+    if conn is not None:
+        # create projects table
+        create_table(conn, sql_create_events_table)
+
+    else:
+        print("Error! cannot create the database connection.")
+
+if __name__ == '__main__':
+    create_connection(nodeName)
