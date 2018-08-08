09bee100946d34e46305b6c13436dd1317cc3567
Julian Stahl <julsjunior@gmail.com>
2018-08-08 17:09:34 -0600

    trying example scripts

---
+++ Python_Learning/ex-database.py
@@ -0,0 +1,17 @@
+import sqlite3
+from sqlite3 import Error
+import os
+nodeName = os.getlogin() + '.db'
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    try:
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+    except Error as e:
+        print(e)
+    finally:
+        conn.close()
+
+if __name__ == '__main__':
+    create_connection(nodeName)
