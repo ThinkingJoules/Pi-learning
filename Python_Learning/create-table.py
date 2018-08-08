235d1b6a0570ed1018562a908f15e46548a20d04
Julian Stahl <julsjunior@gmail.com>
2018-08-08 17:16:48 -0600

    correct database and table creation files

--- Python_Learning/create-table.py
+++ Python_Learning/create-table.py
@@ -1,24 +1,9 @@
 import sqlite3
+from sqlite3 import Error
 import os
-from sqlite3 import Error
 
 
-
-
-
-if conn is not None:
-    conn.execute(sql_create_projects_table)
-else:
-    print("Error! cannot create the database connection.")
-
-conn.close()
-
-
-import sqlite3
-from sqlite3 import Error
-
-
-ddef create_connection(db_file):
+def create_connection(db_file):
     """ create a database connection to the SQLite database
         specified by db_file
     :param db_file: database file
@@ -43,9 +28,10 @@ def create_table(conn, create_table_sql):
         c.execute(create_table_sql)
     except Error as e:
         print(e)
- def main():
-    nodeName = os.getlogin() + '.db'
-    database = nodeName
+
+def main():
+
+    database = os.getlogin() + '.db'
 
     sql_create_events_table = """ CREATE TABLE IF NOT EXISTS Events (
                                             id integer PRIMARY KEY,
@@ -56,14 +42,16 @@ def create_table(conn, create_table_sql):
                                             Duration real,
                                         ); """
 
+
     # create a database connection
     conn = create_connection(database)
     if conn is not None:
         # create projects table
-        create_table(conn, sql_create_events_table)
-
+        create_table(conn, sql_create_projects_table)
+        # create tasks table
+        create_table(conn, sql_create_tasks_table)
     else:
         print("Error! cannot create the database connection.")
 
 if __name__ == '__main__':
-    create_connection(nodeName)
+    main()
