69b766e0be80cbee9e1b489e5cd7845c3c166f33
Julian Stahl <julsjunior@gmail.com>
2018-08-11 21:15:15 +0000

    missed these files in last commit

---
+++ Python_Learning/every_five.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cd /home/manual-mill/Pi-learning/Python_Learning;
+CWD="$(pwd)"
+echo $CWD
+python gsheet-display.py
---
+++ Python_Learning/google_auth.py
@@ -0,0 +1,14 @@
+import gspread
+import os
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+def auth():
+    scope = ['https://spreadsheets.google.com/feeds',
+            'https://www.googleapis.com/auth/drive']
+
+    credentials = ServiceAccountCredentials.from_json_keyfile_name('talk-to-sheets-3d2c8fe70da5.json', scope)
+
+    gc = gspread.authorize(credentials)
+
+    return gc
